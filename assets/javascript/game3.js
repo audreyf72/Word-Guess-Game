6e93124b7351e09016a79de53ccd543fb432b21a
audrey <audreyfernandez@verizon.net>
2018-05-03 20:42:10 -0700

    more changes

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -15,9 +15,17 @@ var remainingLetters = randomWord.length;
 //Display unerscores in game
 document.getElementById("underScores").innerHTML = answers;
 
-//Prompt player to guess a letter
-
+//Process player letter guess
+document.onkeyup= function(event) {
+    var playerGuess = event.key;
+    var playerLetter = String.fromCharCode(event.playerGuess);
+};
 //Check to see if player guess is correct
+function checkGuess()
+    if (playerGuess == randomWord.substring(length)){
+
+    }
+    if (playerGuess <= radomWord.index[-1])
 
 //Prevent player guess of same letter twice
 
@@ -25,11 +33,12 @@ document.getElementById("underScores").innerHTML = answers;
 
 //If player guess is not correct, put letter in letters guessed area and subtract 1 from guesses remaining
 
-//If player completes the entire word add 1 to wins and restart game
+//If player completes the entire word show an image of the character, play soundbite and alert to restart game
 
-//If guesses remaining reaches zero and word is not complete then add 1 to losses and restart game
+//If guesses remaining reaches zero and word is not complete then alert player and restart game
 
-//Display letters guessed in the game so far
+//Display incorrect letters guessed in the game so far
+document.getElementById("lettersGuessed").innerHTML = playerLetter;
 
 //Disply guesses remaining
 document.getElementById("lettersRem").innerHTML = remainingLetters;
