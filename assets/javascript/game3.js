9d6ff4badbc8b5398182515b46930c0962e94522
Art Department | Screen Art <artdept@ArtDept-iMac.local>
2018-05-04 12:04:06 -0700

    4th round of changes

--- assets/javascript/game3.js
+++ assets/javascript/game3.js
@@ -1,4 +1,7 @@
 //Create an array of random words
+var lettersGuessed = [];
+var guessesLeft = 10;
+var wins = 0;
 var words = ["lannister", "stark", "targaryan", "baratheon", "snow", "winterfell", "westeros",
 "aemon", "joffrey", "robert", "stannis", "bolton", "ramsay", "greyjoy", "cersei", "jaime", "tyrion",
 "mormont", "sansa", "arya", "eddard", "rhaegar", "daenerys", "brienne", "highgarden"];
@@ -12,24 +15,45 @@ for (var i = 0; i < randomWord.length; i++) {
 //Keep track of letters left to be guessed
 var remainingLetters = randomWord.length;
 
-//Display unerscores in game
+//Display underscores in game
 document.getElementById("underScores").innerHTML = answers;
 
-//Prompt player to guess a letter
-
+//Player guess
+/*document.onkeyup = function(event) {
+var playerGuess = String.fromCharCode(event.keyCode).toLowerCase();
+if (playerGuess) < 0 && words.indexOf(randomWord) >= 0) {
+    lettersGuessed[lettersGuessed.length]=playerGuess;
+    guessesLeft--;
+}
 //Check to see if player guess is correct
+if (playerGuess.indexOf(playerGuess) === 0 && words.indexOf(randomWord) >= 0) {
+    guessesLeft--;
+    lettersGuessed = [];
+    playerGuess = randomWord[Math.floor(Math.random() * alphabet.length)];
+}
+//Check to see if player guess is wrong
+if (playerGuess < 0) {
+    guessesLeft --;
+    lettersGuessed = [];
+    randomletter = alphabet[Math.floor(Math.random() * alphabet.length)];
+}
 
 //Prevent player guess of same letter twice
 
 //If player guess is correct, put the letter in the index place of corresponding underscore
 
-//If player guess is not correct, put letter in letters guessed area and subtract 1 from guesses remaining
+//If player guess is not correct, put letter in letters guessed area and subtract 1 from guesses left
 
 //If player completes the entire word add 1 to wins and restart game
 
-//If guesses remaining reaches zero and word is not complete then add 1 to losses and restart game
+//If guesses left reaches zero and word is not complete then restart game
 
 //Display letters guessed in the game so far
+document.getElementById("lettersGuessed").innerHTML = lettersGuessed;*/
 
-//Disply guesses remaining
-document.getElementById("lettersRem").innerHTML = remainingLetters;
+//Display guesses left
+document.getElementById("lettersRem").innerHTML = guessesLeft;
+
+//Display player wins
+var htmlWins = "<p>Player Wins: " + wins + "</p>"
+document.querySelector("#wins").innerHTML = htmlWins;
