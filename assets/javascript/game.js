f74e2fff6e90a8dd3e6d39510b16096d81a23f4f
audrey <audreyfernandez@verizon.net>
2018-05-05 20:58:05 -0700

    Final Homework Submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,40 +1,84 @@
-//Generate random words
-var words = [
-    "beach",
-    "mountain",
-    "bicycle",
-    "swimming",
-    "waterslide",
-    "playground",
-    "tennis",
-    "volleyball"];
+//Create an array of random words
+var words = ["joffrey", "robert", "jon", "margaery", "theon", "cersei", "jaime", "tyrion",
+"sansa", "arya", "ygritte", "daenerys", "brienne", "catelyn", "melissandre", "littlefinger", "varys"];
 
-var underScores = [];
+//Create variables for Letters Guessed, Guesses Left and Player Wins
+var lettersGuessed = [];
+var guessesLeft = 10;
+var wins = 0;
+var playerGuess = event;
+var wordPic = "Valar Morghulis";
 
-for (var i = 0; i < random.length; i++){
-    var random = random[Math.floor(Math.random() * words.length)];
-    var underScoresRem = random.length;
-    underScores[i] = "_";
+//Create a function that will select the random word for the game
+var randomWord = words[Math.floor(Math.random() * words.length)];
+
+//Generate underscores corresponding to the length of the random word chosen
+var answers = [];
+for (var i = 0; i < randomWord.length; i++) {
+    answers[i] = "_";
 }
+console.log(randomWord);
 
-while (underScoresRem > 0) {
-    
-alert(underScores.join(" "));
+//Keeps track of letters left to be guessed
+var remainingLetters = randomWord.length;
 
-var playerGuess = prompt("Guess a letter");
-    if (guess === null){
-        break;
-    } else if (guess.length !== 1) {
-        alert("Please enter a single letter");
-    } else{
-        for (var j = 0; j < random.length; j++){
-            if (random[j] === playerGuess) {
-                underScores[j] = guess;
-                underScoresRem--;
+//Display underscores in game (Result? Underscores display with the correct number of spaces for the randomWord but doesn't look right)
+document.getElementById("underScores").innerHTML = answers;
+console.log(answers);
+
+//Player guess
+// Determines which key was pressed (This is where I got stuck and couldn't figure out how to fix code beyond this point)
+document.onkeyup = function (event) {
+    var playerGuess = String.fromCharCode(event.onkeyup).toLowerCase();
+
+//Check to see if player guess is correct
+    if (playerGuess.indexOf(randomWord) > -1) {
+        document.getElementById(lettersGuessed).innerHTML += playerGuess;
+
+//If player guess is correct, put the letter in the index place of corresponding underscore        
+        var targetDiv = document.getElementById("underScores");
+        targetDiv.textContent = "";
+        var newDiv = document.createElement("div");
+        newDiv.textContent = playerGuess;
+        
+//If player completes the entire word add 1 to wins, change image, image title, and restart game        
+        wins ++;
+        guessesLeft--;
+        lettersGuessed = [];
+        if (wins == 6) {
+            inititalizGame();
         }
+//Check to see if player guess is wrong    
+//If player guess is not correct, put letter in letters guessed area and subtract 1 from guesses left    
+    } else {
+        lettersGuessed = [];
+        guessesleft--;
+
+//If guesses left reaches zero and word is not complete then restart game        
+        if (guessesLeft == 0) {
+            inititalizGame();
+        } else {
+            document.getElementById(lettersGuessed).innerHTML += playerGuess;
+            var targetDiv = document.getElementById("lettersGuessed");
+            targetDiv.textContent = "";
+            var newDiv = document.createElement("div");
+            newDiv.textContent = playerGuess;
+        }
+
     }
-    };
+}
+//Prevent player guess of same letter twice
 
+//Display letters guessed in the game so far
+var lettersGuessed = document.getElementById("lettersGuessed");
+lettersGuessed.textContent = lettersGuessed;
 
+//Display guesses left
+document.getElementById("lettersRem").innerHTML = guessesLeft;
 
+//Display player wins
+var htmlWins = "<p>Player Wins: " + wins + "</p>"
+document.querySelector("#wins").innerHTML = htmlWins;
 
+var wordPicHTML = "<p>" + wordPic + "</p>"
+document.querySelector("#wordpicTitle").innerHTML = wordPicHTML;
