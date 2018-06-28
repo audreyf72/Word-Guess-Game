f97f6e720db4538fc22bba954565bb1bf5f5ec3d
Art Department | Screen Art <artdept@ArtDept-iMac.local>
2018-06-28 16:07:58 -0700

    replaced images

--- assets/javascript/game4.js
+++ assets/javascript/game4.js
@@ -1,3 +1,5 @@
+
+
 // Creating a giant wordGuessGame object that will house all of our logic and variables.
 var wordGuessGame = {
 
@@ -309,4 +311,29 @@ var wordGuessGame = {
     // Pass the guessed letter into our updatePage function to run the game logic.
     wordGuessGame.updatePage(wordGuessGame.letterGuessed);
   };
-  
+
+  // Get the modal
+var modal = document.getElementById('myModal');
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+    modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+    modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
